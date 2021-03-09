170329abe09955275c2822424e826ed4258eecc3
Swamdidam <Swamdidam@gmail.com>
2021-03-09 12:11:58 +0100

    Update date_and_time_formating.js

--- date_and_time_formating.js
+++ date_and_time_formating.js
@@ -3,3 +3,7 @@ new Date().toLocaleTimeString();
 new Date().toLocaleDateString(); 
 
 new Date().toLocaleString();
+
+// adding 4 hours later (use milisec: sec==1000):
+
+new Date(new Date().getTime() + 4*60*60*1000).toLocaleTimeString()
